ae0c34724f6922b090c221e63d58227d9a875c68
Sander <Sander@SANDERMOLEN8649>
2017-01-05 07:03:20 +0100

    Added SignalR

---
+++ src/ServiceBusUI/Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.SignalR;
+
+namespace ServiceBusUI.Controllers
+{
+    public class MessageController : ApiController
+    {
+        public void Post([FromBody]string value)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<MessageHub>();
+
+            context.Clients.All.newMessage("hello world");
+        }
+    }
+}
