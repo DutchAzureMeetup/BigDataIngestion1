a0b9c70df7d84d403a568db7cdfb011b917b697c
Pascal Naber <pascal.naber@gmail.com>
2017-01-09 22:10:15 +0100

    Support for a connectionstring as a parameter

--- src/src/ThermostatDataGenerator/Program.cs
+++ src/src/ThermostatDataGenerator/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Amqp;
 using Amqp.Framing;
-using CommandLine;
 using Newtonsoft.Json;
 using System;
 using System.Text;
@@ -13,10 +12,42 @@ namespace ThermostatDataGenerator
     {
         public static void Main(string[] args)
         {
-            var commandLineArguments = (Parsed<Options>)Parser.Default.ParseArguments<Options>(args);
+            if (args.Length == 0)
+                throw new Exception("Pass a connectionstring to the Event Hub");
 
-            Task t = MainAsync(commandLineArguments.Value);
-            t.Wait();
+            try
+            {
+                Task t = MainAsync(CreateOptions(args[0]));
+                t.Wait();
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+            }
+        }
+
+        static Options CreateOptions(string connectionString)
+        {
+            Options options = new Options();
+            options.CustomerId = new Random().Next(100, 999).ToString();
+            options.EventHubName = GetConnectionStringPart("EntityPath=", connectionString);
+            options.Namespace = GetConnectionStringPart("Endpoint=", connectionString).Replace("sb://", "").Split('.')[0];
+            options.PolicyName = GetConnectionStringPart("SharedAccessKeyName=", connectionString);
+            options.SasToken = GetConnectionStringPart("SharedAccessKey=", connectionString);
+            return options;
+        }
+
+        static string GetConnectionStringPart(string partName, string connectionString)
+        {
+            int indexOfPart = connectionString.IndexOf(partName);
+            int indexOfPartEnd = connectionString.IndexOf(";", indexOfPart);
+            if (indexOfPartEnd == -1) // in case of EntityPath there is no ;
+                indexOfPartEnd = connectionString.Length;
+
+            int indexOfBeginPartValue = indexOfPart + partName.Length;
+            int lengthOfPartValue = indexOfPartEnd - indexOfBeginPartValue;
+
+            return connectionString.Substring(indexOfBeginPartValue, lengthOfPartValue);
         }
 
         static async Task MainAsync(Options options)
@@ -28,8 +59,16 @@ namespace ThermostatDataGenerator
             string connectionString = $"amqps://{policyName}:{sasToken}@{namespaceUrl}/";
 
             Address address = new Address(connectionString);
+            Connection connection;
+            try
+            {
+                connection = await Connection.Factory.CreateAsync(address);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"The namespace {options.Namespace} is probably wrong.", ex);
+            }
 
-            Connection connection = await Connection.Factory.CreateAsync(address);
             Session session = new Session(connection);
 
             Random rnd = new Random(Guid.NewGuid().GetHashCode());
@@ -45,37 +84,54 @@ namespace ThermostatDataGenerator
                     ElectricityUsage = rnd.Next(0, 500),
                     CustomerId = options.CustomerId
                 };
+
                 string serializedJson = JsonConvert.SerializeObject(data);
 
                 Message message = new Message
                 {
-                    BodySection = new Data {Binary = Encoding.UTF8.GetBytes(serializedJson)}
+                    BodySection = new Data { Binary = Encoding.UTF8.GetBytes(serializedJson) }
                 };
 
                 SenderLink sender = new SenderLink(session, "sender-link", options.EventHubName);
 
-                await sender.SendAsync(message);
+                try
+                {
+                    await sender.SendAsync(message);
+                }
+                catch (AmqpException ex)
+                {
+                    if (ex.Error.Condition.ToString().Contains("amqp:unauthorized-access"))
+                    {
+                        throw new Exception($"The policyname {options.PolicyName} or the SAS token {options.SasToken} is probably wrong.", ex);
+                    }
+                    else if (ex.Error.Condition.ToString().Contains("amqp:not-found"))
+                    {
+                        throw new Exception($"The eventhub name {options.EventHubName} is probably wrong.", ex);
+                    }
+                }
+
 
                 await sender.CloseAsync();
             }
+
         }
     }
 
     class Options
     {
-        [Option('n', "namespace", HelpText = "Name of the Namespace of the EventHub. Only the name (without .servicebus.windows.net)", Required = true)]
+      
         public string Namespace { get; set; }
 
-        [Option('p', "policyname", HelpText = "Name of the Policy which has Send rights on the Hub", Required = true)]
+      
         public string PolicyName { get; set; }
 
-        [Option('e', "eventhubname", HelpText = "Name of the Event Hub", Required = true)]
+      
         public string EventHubName { get; set; }
 
-        [Option('s', "sastoken", HelpText = "Sastoken to access to Event Hub", Required = true)]
+      
         public string SasToken { get; set; }
 
-        [Option('c', "customerid", HelpText = "CustomerId which you will find back in the data on the EventHub", Required = true)]
+      
         public string CustomerId { get; set; }
     }
 
